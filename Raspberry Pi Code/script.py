dd86c3d75052b712507b7443b374805718f11a65
Yuan Zhang <mikebwin@gmail.com>
2019-02-04 11:50:01 -0500

    added currently running script on RPi

---
+++ Raspberry Pi Code/script.py
@@ -0,0 +1,63 @@
+import os
+import glob
+import time
+from bluetooth import *
+
+os.system("sudo hciconfig hci0 piscan")
+os.system("sudo hciconfig hci0 name smartbox_pi_demo")
+os.system("sudo sdptool add --channel=1 SP")
+
+#while True:
+#	print(read_temp())	
+#	time.sleep(1)
+
+
+server_sock=BluetoothSocket( RFCOMM )
+server_sock.bind(("",PORT_ANY))
+server_sock.listen(1)
+
+port = server_sock.getsockname()[1]
+
+uuid = "94f39d29-7d6d-437d-973b-fba39e49d4ee"
+
+advertise_service( server_sock, "smartlock_pi_demo",
+                   service_id = uuid,
+                   service_classes = [ uuid, SERIAL_PORT_CLASS ],
+                   profiles = [ SERIAL_PORT_PROFILE ], 
+#                   protocols = [ OBEX_UUID ] 
+                    )
+while True:          
+	print("Waiting for connection on RFCOMM channel %d" % port)
+
+	client_sock, client_info = server_sock.accept()
+	print("Accepted connection from ", client_info)
+
+
+	try:
+	    data = client_sock.recv(1024)
+	    while data != "quit":
+                if len(data) == 0: 
+                    client_sock.send("empty string")
+
+#                print(data)
+ #               cmd_array = data.split()
+
+	    #if statements here
+                if data != "quit":
+                    print data
+                    client_sock.send("data received")
+
+                if data == "quit":
+                    break
+	        
+                data = client_sock.recv(1024)
+
+	except IOError:
+	    pass
+
+	except KeyboardInterrupt:
+            client_sock.close()
+            server_sock.close()
+            break
+
+	    
