9eff477d522e6f66bc31e504366754ff647ff4a5
mikebwin <mikebwin@gmail.com>
2019-03-07 20:31:13 -0500

    cleaned up WifiInput. look at wpa_supplicant and create bash script for wifi conections

--- Bluetooth_Demo/app/src/main/java/smartbox/bluetoothdemo/WifiInput.java
+++ Bluetooth_Demo/app/src/main/java/smartbox/bluetoothdemo/WifiInput.java
@@ -5,6 +5,7 @@ import java.io.InputStream;
 import java.io.OutputStream;
 import java.util.Set;
 import java.util.UUID;
+import java.util.concurrent.atomic.AtomicReference;
 
 import android.app.Activity;
 import android.bluetooth.BluetoothAdapter;
@@ -23,13 +24,10 @@ import android.widget.Spinner;
 
 public class WifiInput extends Activity {
 
-    BluetoothSocket mSocket;
-    BluetoothDevice mDevice = null;
-
     final byte delimiter = 33;
     int readBufferPosition = 0;
 
-    private BluetoothAdapter mBluetoothAdapter;
+    BluetoothSingleton single = new BluetoothSingleton();
 
     private EditText mPasswordView;
     private AutoCompleteTextView mWifiName;
@@ -41,38 +39,37 @@ public class WifiInput extends Activity {
 
     private UUID uuid;
 
-    private void bluetoothInit(){
-
-        mBluetoothAdapter = BluetoothAdapter.getDefaultAdapter();
-
-        if(!mBluetoothAdapter.isEnabled())
-        {
-            Intent enableBluetooth = new Intent(BluetoothAdapter.ACTION_REQUEST_ENABLE);
-            startActivityForResult(enableBluetooth, 0);
-        }
-
-        Set<BluetoothDevice> pairedDevices = mBluetoothAdapter.getBondedDevices();
-        if(pairedDevices.size() > 0)
-        {
-            for(BluetoothDevice device : pairedDevices)
-            {
-                if(device.getName().contains("smartbox")) //Note, you will need to change this to match the name of your device
-                {
-                    mDevice = device;
-                    uid_list = mDevice.getUuids();
-
-                    try {
-                        mSocket = mDevice.createRfcommSocketToServiceRecord(uid_list[uid_list.length - 1].getUuid());
-                        mSocket.connect();
-                        uuid = uid_list[uid_list.length - 1].getUuid();
-                    } catch (IOException e) {
-                    }
-
-                    break;
-                }
-            }
-        }
-    }
+//    private void bluetoothInit(){
+//
+//
+//        if(!single.getAdapter().isEnabled())
+//        {
+//            Intent enableBluetooth = new Intent(BluetoothAdapter.ACTION_REQUEST_ENABLE);
+//            startActivityForResult(enableBluetooth, 0);
+//        }
+//
+//        Set<BluetoothDevice> pairedDevices = single.getAdapter().getBondedDevices();
+//        if(pairedDevices.size() > 0)
+//        {
+//            for(BluetoothDevice device : pairedDevices)
+//            {
+//                if(device.getName().contains("smartbox")) //Note, you will need to change this to match the name of your device
+//                {
+//                    mDevice = device;
+//                    uid_list = mDevice.getUuids();
+//
+//                    try {
+//                        mSocket = mDevice.createRfcommSocketToServiceRecord(uid_list[uid_list.length - 1].getUuid());
+//                        mSocket.connect();
+//                        uuid = uid_list[uid_list.length - 1].getUuid();
+//                    } catch (IOException e) {
+//                    }
+//
+//                    break;
+//                }
+//            }
+//        }
+//    }
 
     public String receiveData(BluetoothSocket socket) throws IOException{
         InputStream socketInputStream =  socket.getInputStream();
@@ -95,22 +92,22 @@ public class WifiInput extends Activity {
         return readMessage;
     }
 
-    public String sendBluetoothMessage(String messangeToSend, EditText textField) {
+    public String sendBluetoothMessage(String messangeToSend) {
 //        UUID uuid = UUID.fromString("94f39d29-7d6d-437d-973b-fba39e49d4ee"); //Standard SerialPortService ID
 //        mErrorText.setText("trying to send message: " + messangeToSend);
 
         try {
 //            mSocket = mmDevice.createRfcommSocketToServiceRecord(uuid);
-            if (!mSocket.isConnected()){
-                mSocket.connect();
+            if (!single.getSocket().isConnected()){
+                single.getSocket().connect();
             }
 
             String msg = messangeToSend;
             //msg += "\n";
             try {
-                OutputStream mmOutputStream = mSocket.getOutputStream();
+                OutputStream mmOutputStream = single.getSocket().getOutputStream();
                 mmOutputStream.write(msg.getBytes());
-                return receiveData(mSocket);
+                return receiveData(single.getSocket());
             } catch (IOException ioEx) {
             }
 
@@ -126,22 +123,10 @@ public class WifiInput extends Activity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
 
-        Bundle b = this.getIntent().getExtras();
-        if (b != null)
-            mDevice = b.getParcelable("bluetooth_device");
-
         wifi_name = (String) getIntent().getSerializableExtra("wifi_name");
         String security = (String) getIntent().getSerializableExtra("security");
 
-        try {
-            mSocket = mDevice.createRfcommSocketToServiceRecord(mDevice.getUuids()[mDevice.getUuids().length - 1].getUuid());
-        } catch (IOException e) {
-            System.out.println("socket issue");
-        }
-
         setContentView(R.layout.activity_wifi_input);
-
-        final Handler handler = new Handler();
 
         mPasswordView = findViewById(R.id.password);
         mWifiName = findViewById(R.id.wifi);
@@ -153,6 +138,9 @@ public class WifiInput extends Activity {
             mPasswordView.setVisibility(View.GONE);
         }
 
+        final AtomicReference wifiString = new AtomicReference();
+
+        final Handler handler = new Handler();
         final class workerThread implements Runnable {
 
             private String btMsg;
@@ -163,54 +151,35 @@ public class WifiInput extends Activity {
 
             public void run()
             {
-                while(!Thread.currentThread().isInterrupted())
-                {
-                    int bytesAvailable;
-                    boolean workDone = false;
-                    try {
-                        final InputStream mmInputStream;
-                        mmInputStream = mSocket.getInputStream();
-                        bytesAvailable = mmInputStream.available();
-                        if(bytesAvailable > 0)
-                        {
-                            byte[] packetBytes = new byte[bytesAvailable];
-                            byte[] readBuffer = new byte[1024];
-                            mmInputStream.read(packetBytes);
+                // here is the call to send a message
+                wifiString.set(sendBluetoothMessage(btMsg));
+//                while(!Thread.currentThread().isInterrupted())
+//                {
+                int bytesAvailable;
+                boolean workDone = false;
+                try {
+                    final InputStream mmInputStream;
+                    mmInputStream = single.getSocket().getInputStream();
+                    bytesAvailable = mmInputStream.available();
 
-                            for(int i=0;i<bytesAvailable;i++)
-                            {
-                                byte b = packetBytes[i];
-                                if(b == delimiter)
-                                {
-                                    byte[] encodedBytes = new byte[readBufferPosition];
-                                    System.arraycopy(readBuffer, 0, encodedBytes, 0, encodedBytes.length);
-                                    final String data = new String(encodedBytes, "US-ASCII");
-                                    readBufferPosition = 0;
+                    if(bytesAvailable > 0)
+                    {
+                        final int BUFFER_SIZE = 10000;
+                        byte[] buffer = new byte[BUFFER_SIZE];
+                        int bytes = 0;
 
-                                    //The variable data now contains our full command
-                                    handler.post(new Runnable()
-                                    {
-                                        public void run()
-                                        {
-                                        }
-                                    });
-
-                                    workDone = true;
-                                    break;
-                                }
-                                else
-                                {
-                                    readBuffer[readBufferPosition++] = b;
-                                }
-                            }
-                            if (workDone == true){
-                                mSocket.close();
+                        while (true) {
+                            try {
+                                bytes = mmInputStream.read(buffer, bytes, BUFFER_SIZE - bytes);
+                            } catch (IOException e) {
+                                System.out.println("exception");
                                 break;
                             }
                         }
-                    } catch (IOException e) {
-                        // TODO Auto-generated catch block
                     }
+                } catch (IOException e) {
+                    // TODO Auto-generated catch block
+                    System.out.println("Thread Issue");
                 }
             }
         };
